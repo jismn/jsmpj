9bad0d9d02e9836d3f956218d6d98939314deb13
jsm <jsm@192.168.219.106>
2022-01-28 03:29:57 +0900

    20220128

---
+++ Shop_pj/src/main/java/com/jsm/service/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package com.jsm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jsm.mapper.MemberMapper;
+import com.jsm.model.MemberVO;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	MemberMapper membermapper;
+
+	/* 회원가입 */
+	@Override
+	public void memberJoin(MemberVO member) throws Exception {
+		
+		membermapper.memberJoin(member);
+		
+	}
+
+	/* 아이디 중복 검사 */
+	@Override
+	public int idCheck(String memberId) throws Exception {
+		
+		return membermapper.idCheck(memberId);
+	}
+
+	/* 로그인 */
+	@Override
+	public MemberVO memberLogin(MemberVO member) throws Exception {
+		
+		return membermapper.memberLogin(member);
+	}
+	
+	/* 주문자 정보 */
+	@Override
+	public MemberVO getMemberInfo(String memberId) {
+		
+		return membermapper.getMemberInfo(memberId);
+		
+	}	
+
+}
